3155f1052a10543271ea4eab7097fbe2f2306b9c
Lucas Horta <lucashorta@gmail.com>
2021-08-20 21:04:09 -0300

    otimizando imports

--- src/main/java/com/ia/desafioia/DesafioIaApplication.java
+++ src/main/java/com/ia/desafioia/DesafioIaApplication.java
@@ -1,11 +1,7 @@
 package com.ia.desafioia;
 
-import com.ia.desafioia.models.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class DesafioIaApplication {
